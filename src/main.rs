2e50b4a4817cced94edf694f27a7ce394a1201b3
AuroraEchoes <auroraesmerelda@gmail.com>
2023-10-24 21:15:02 +1100

    Cursory Discord integration:wq

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, time::Duration, thread};
+use anyhow::Result;
+use discord_presence::{Client, Event};
+
+fn main() -> Result<()>{
+    dotenv::dotenv().ok();
+
+    let waka_api_key = env::var("WAKA_API_KEY")?;
+
+    let mut client = Client::new(1113765598602727455);
+    client.on_event(Event::Ready, |_| {
+        println!("READY!");
+    });
+    let _ = client.start();
+    client.block_until_event(Event::Ready)?;
+    client.set_activity(|build| build.details("Total time: X // Project time: X").state("Working on discord_code_time"))?;
+    thread::sleep(Duration::from_secs(30));
+
+    return Ok(());
 }
