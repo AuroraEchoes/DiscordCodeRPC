5601fdc4a5f0b0ea8972d8f887e855ef948dbf73
AuroraEchoes <auroraesmerelda@gmail.com>
2023-10-25 18:53:19 +1100

    Functional WakaTime integration

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ fn main() -> Result<()>{
     dotenv::dotenv().ok();
 
     let waka_api_key = env::var("WAKA_API_KEY")?;
+    let waka_user_id = env::var("WAKA_USER_ID")?;
+
+    let response = reqwest::blocking::get(
+        format!("https://wakatime.com/api/v1/users/current/stats?api_key={waka_api_key}")
+    )?;
+    let json = json::parse(response.text()?.as_str())?;
+    let daily_average = &json["data"]["human_readable_daily_average"].as_str().unwrap();
 
     let mut client = Client::new(1113765598602727455);
     client.on_event(Event::Ready, |_| {
@@ -13,7 +20,7 @@ fn main() -> Result<()>{
     });
     let _ = client.start();
     client.block_until_event(Event::Ready)?;
-    client.set_activity(|build| build.details("Total time: X // Project time: X").state("Working on discord_code_time"))?;
+    client.set_activity(|build| build.details(format!("Daily average: {daily_average}")).state("Working on discord_code_time"))?;
     thread::sleep(Duration::from_secs(30));
 
     return Ok(());
